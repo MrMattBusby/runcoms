f57aa1e02e2f60d995cbb07b4141943ed4b5cb85
Matt Busby <OOo01olliIIl10lOiijiii0l11@gmail.com>
2023-04-19 09:10:43 -0400

    Update .pythonrc.py

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# See dunder variables for LICENSE and COPYRIGHT.
 """Python run command. Please see metadata (via pydoc) for further info.
 
 LICENSE
@@ -18,50 +19,18 @@ from __future__ import print_function, with_statement, division
 # Metadata, used in $ pydoc ./%
 __creator__ = "Matt Busby"
 __email__ = "@MrMattBusby"
-__date__ = "2010-2016"
-
+__date__ = "2010"
 __version__ = "See GitHub"
-__project__ = "https://github.com/MrMattBusby/"
-
+__project__ = "https://github.com/MrMattBusby/runcoms"
 __author__ = "{0} {1}".format(__creator__, __email__)
-__copyright__ = "Copyright (c) {year}, {owner}. ".format(
+__copyright__ = "Copyright (c) {year} {owner}".format(
                         owner = __author__,
-                        year = "2012") + \
+                        year = __date__) + \
                    "All rights reserved."
-__licence__ = """\
-        BSD 3-Clause License
-
-        {copyright}
-
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions
-        are met:
-
-        1. Redistributions of source code must retain the above copyright
-           notice, this list of conditions and the following disclaimer.
-
-        2. Redistributions in binary form must reproduce the above copyright
-           notice, this list of conditions and the following disclaimer in
-           the documentation and/or other materials provided with the
-           distribution.
-
-        3. Neither the name of the copyright holder nor the names of its
-           contributors may be used to endorse or promote products derived
-           from this software without specific prior written permission.
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-        LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-        FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-        COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-        INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-        BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-        CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-        LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-        WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-        POSSIBILITY OF SUCH DAMAGE.
-        """.format(copyright=__copyright__)
+__licence__ = """The MIT License
+{copyright}
+See: https://github.com/MrMattBusby/runcoms/blob/master/LICENSE
+""".format(copyright=__copyright__)
 
 ######################## Imports/Globals ######################################
 
